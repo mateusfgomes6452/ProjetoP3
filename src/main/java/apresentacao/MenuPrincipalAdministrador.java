a4795bee8813d0239d1321a1f2d243a25e0c47e2
Mateus6452 <mateus.fgomes@upe.br>
2026-09-22 04:27:27 -0300

    fix/Para de reclamar do Sysout.println() na classe MenuPrincipalAdministrador.java

--- src/main/java/apresentacao/MenuPrincipalAdministrador.java
+++ src/main/java/apresentacao/MenuPrincipalAdministrador.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import entidades.Administrador;
 import facade.SistemaFacade;
 
+@SuppressWarnings("java:S106")
 public class MenuPrincipalAdministrador {
 
     private SistemaFacade sistema;
