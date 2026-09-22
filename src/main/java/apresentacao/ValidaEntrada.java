95dbf16b84e905a3e61a7b4c22e75e73ab760dea
Arthur Dias <arthurdiasm28@gmail.com>
2026-09-21 22:02:20 -0300

    fix: corrigindo while true

--- src/main/java/apresentacao/ValidaEntrada.java
+++ src/main/java/apresentacao/ValidaEntrada.java
@@ -108,24 +108,22 @@ public class ValidaEntrada {
     public static String lerTelefone(Scanner scanner) {
     	Pattern padraoTelefoneCelular = Pattern.compile("\\([0-9]{2}\\) 9 [0-9]{4}\\-[0-9]{4}");
     	Pattern padraoTelefoneFixo = Pattern.compile("\\([0-9]{2}\\) [0-9]{4}\\-[0-9]{4}");
-    	
-    	boolean continuar = true;
-    	
-    	while (continuar) {
-    		String entrada = scanner.nextLine().trim();
-    		
-    		if (entrada.isEmpty()) {
-                return "";
-            }
-    		
-    		Matcher matcherCelular = padraoTelefoneCelular.matcher(entrada);
-    		Matcher matcherFixo = padraoTelefoneFixo.matcher(entrada);
-    		
-    		if (matcherCelular.matches() || matcherFixo.matches()) {
-    			return entrada;
-    		}
-    		System.out.print("Formato inválido. Use (DDD) 9 0000-0000 ou (DDD) 0000-0000: ");
+
+    	while (true) {
+        	String entrada = scanner.nextLine().trim();
+
+        	if (entrada.isEmpty()) {
+            	return "";
+        	}
+
+        	Matcher matcherCelular = padraoTelefoneCelular.matcher(entrada);
+        	Matcher matcherFixo = padraoTelefoneFixo.matcher(entrada);
+
+        	if (matcherCelular.matches() || matcherFixo.matches()) {
+            	return entrada;
+        	}
+
+        	System.out.print("Formato inválido. Use (DDD) 9 0000-0000 ou (DDD) 0000-0000: ");
     	}
-    	return "";
-    }
+	}
 }
