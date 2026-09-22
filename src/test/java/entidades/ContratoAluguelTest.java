b88e6aa29fc9061102458678f5288547133f826e
Mateus6452 <mateus.fgomes@upe.br>
2026-09-22 06:25:16 -0300

    Fix: Removendo import não utilizado na classe ContratoAluguelTest.java

--- src/test/java/entidades/ContratoAluguelTest.java
+++ src/test/java/entidades/ContratoAluguelTest.java
@@ -1,6 +1,5 @@
 package entidades;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
