05759156f4a0febd1458c7a3b4027a96d2b5fbdd
Arthur Dias <arthurdiasm28@gmail.com>
2026-09-21 22:27:42 -0300

    fix concertando regex

--- src/main/java/apresentacao/ValidaEntrada.java
+++ src/main/java/apresentacao/ValidaEntrada.java
@@ -104,8 +104,8 @@ public class ValidaEntrada {
     }
     
     public static String lerTelefone(Scanner scanner) {
-    	Pattern padraoTelefoneCelular = Pattern.compile("\\([0-9]{2}\\) 9 [0-9]{4}\\-[0-9]{4}");
-    	Pattern padraoTelefoneFixo = Pattern.compile("\\([0-9]{2}\\) [0-9]{4}\\-[0-9]{4}");
+    	Pattern padraoTelefoneCelular = Pattern.compile("\\(\\d{2}\\) 9 \\d{4}-\\d{4}");
+    	Pattern padraoTelefoneFixo = Pattern.compile("\\(\\d{2}\\) \\d{4}-\\d{4}");
 
     	while (true) {
         	String entrada = scanner.nextLine().trim();
